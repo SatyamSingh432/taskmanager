9905b189f78ec5c854b31c5cec75e3e8369c48f6
Satyam Singh <satyamrajput7239@gmail.com>
2025-08-03 08:15:49 +0530

    feat : Add Task model

---
+++ backend/src/models/Task.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  status: { type: String, enum: ["TODO", "DONE"], default: "TODO" },
+  linkedFile: {
+    type: "Buffer",
+    contentType: "application/pdf",
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  deadline: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
