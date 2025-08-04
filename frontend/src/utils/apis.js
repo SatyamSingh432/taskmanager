5aaa1163488c2cf6f02b132fd5c1899b52839462
Satyam Singh <satyamrajput7239@gmail.com>
2025-08-04 17:19:11 +0530

    feat: Add CURD apis

---
+++ frontend/src/utils/apis.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8082/v1";
+
+export const createTask = async (taskData) => {
+  try {
+    const formData = new FormData();
+    formData.append("title", taskData.title);
+    formData.append("description", taskData.description);
+    formData.append("deadline", taskData.deadline);
+    formData.append("file", taskData.file);
+
+    const response = await axios.post(`${BASE_URL}/`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(" Error creating task:", error.message);
+  }
+};
+
+export const getTasks = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/`);
+    return response.data;
+  } catch (error) {
+    console.error(" Error fetching tasks:", error.message);
+  }
+};
+
+export const updateTask = async (id, taskData) => {
+  try {
+    const response = await axios.put(`${BASE_URL}/${id}`, {
+      title: taskData.title,
+      description: taskData.description,
+      deadline: taskData.deadline,
+      file: taskData.file,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(` Error updating task :`, error.message);
+  }
+};
+
+export const deleteTask = async (id) => {
+  try {
+    await axios.delete(`${BASE_URL}/${id}`);
+    console.log(`Task deleted`);
+  } catch (error) {
+    console.error(`Error deleting task :`, error.message);
+  }
+};
